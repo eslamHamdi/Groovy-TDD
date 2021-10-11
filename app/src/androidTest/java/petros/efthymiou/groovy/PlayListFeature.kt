8f7b8222f111bf4a73f603c73c513ba1704e0bdf
eslamHamdi <eslam.hamdi.hassan@gmail.com>
2021-10-11 23:59:54 +0200

    7th commit - first feature done with green testing suite

--- app/src/androidTest/java/petros/efthymiou/groovy/PlayListFeature.kt
+++ app/src/androidTest/java/petros/efthymiou/groovy/PlayListFeature.kt
@@ -8,8 +8,11 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.adevinta.android.barista.assertion.BaristaRecyclerViewAssertions.assertRecyclerViewItemCount
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.adevinta.android.barista.internal.matcher.DrawableMatcher.Companion.withDrawable
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.InternalCoroutinesApi
 import org.hamcrest.CoreMatchers.allOf
+import org.junit.Before
 
 
 import org.junit.Test
@@ -22,22 +25,31 @@ import petros.nthChildOf
 
 @RunWith(AndroidJUnit4::class)
 @InternalCoroutinesApi
+//@HiltAndroidTest
 class PlayListFeature {
 
-
+    //@Rule val hiltRule: HiltAndroidRule = HiltAndroidRule(this)
 
     @InternalCoroutinesApi
     val mActivity = ActivityScenarioRule(MainActivity::class.java)
         @Rule get
+
+    @Before
+    fun initialize()
+    {
+       // hiltRule.inject()
+    }
     @Test
     fun displayScreenTitle() {
         assertDisplayed("PlayLists")
     }
 
 
+
     @Test
     fun checkIfListIsDisplayed()
     {
+        Thread.sleep(4000)
        assertRecyclerViewItemCount(R.id.playList_recycler,10)
 
         onView(allOf(withId(R.id.playList_name), isDescendantOfA(nthChildOf(withId(R.id.playList_recycler),0))))
